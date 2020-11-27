d1ff529b2d84ed9f3309cb93b9089884ef4dc2e5
DevNode <thomasluca008@gmail.com>
2020-11-27 17:26:23 +0100

    made createArena() and getPoint() methods in Arena

--- src/lib/arena.cpp
+++ src/lib/arena.cpp
@@ -1,5 +1,18 @@
 #include "arena.h"
 
 namespace Tetris{
+    
+    void Arena::createArena(){
+        for (int i = 0;  i < arenaHeight; i++)
+        {
+            for (int j = 0; j < arenaWidth; j++)
+            {
+                arena[i][j] = 0;
+            }
+        }
+    }
 
+    bool Arena::getPoint(int y, int x){
+        return arena[y][x];
+    }
 };
