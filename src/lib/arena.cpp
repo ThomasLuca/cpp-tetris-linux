0410a7b6f6fed898027d2b64bf58fcb1c5530764
DevNode <thomasluca008@gmail.com>
2020-11-27 17:35:58 +0100

    added lockPiece method to Arena (places piece in arena)

--- src/lib/arena.cpp
+++ src/lib/arena.cpp
@@ -2,6 +2,7 @@
 
 namespace Tetris{
     
+    /*  Initializes an empty arena (every place = False)    */
     void Arena::createArena(){
         for (int i = 0;  i < arenaHeight; i++)
         {
@@ -12,7 +13,22 @@ namespace Tetris{
         }
     }
 
+    /*  Returns boolean value of any given place in the arena   */
     bool Arena::getPoint(int y, int x){
         return arena[y][x];
+    }
+
+    /*  Places pieces anywhere in the arena by enabling certain positions   */
+    void Arena::lockPiece(int y, int x, int piece, int rotation){
+        for (int i = y; i < y + 4; i++)
+        {
+            for (int j = x; j < x + 4; j++)
+            {
+                if (pieces.getBlockOfPiece(piece, rotation, i - y, j - x))
+                {
+                    arena[i][j] = 1;
+                }
+            }
+        }
     }
 };
