5d3fe765ddc0453a70c407aaa10dbd051124feae
DevNode <thomasluca008@gmail.com>
2020-12-16 12:56:51 +0100

    improved code in colors class

--- src/lib/colors.cpp
+++ src/lib/colors.cpp
@@ -1,29 +1,15 @@
 #include "colors.h"
+#include <string>
 
 namespace Tetris{
     std::string Colors::setChar(int colorMoving, int colorLocked){
         determineActivePiece(colorMoving, colorLocked);
-        switch (this->color)
+        if (this->color == 0)
         {
-        case 0:
             return ". ";
-        case 1:
-            return "\033[;31m██\033[0m";
-        case 2:
-            return "\033[;32m██\033[0m";
-        case 3:
-            return "\033[;33m██\033[0m";
-        case 4:
-            return "\033[;34m██\033[0m";
-        case 5:
-            return "\033[;35m██\033[0m";
-        case 6:
-            return "\033[;36m██\033[0m";
-        case 7:
-            return "\033[;37m██\033[0m";
-        default:
-            return "\033[;32m██\033[0m";
-        }
+        } else {
+            return "\033[;3" + std::to_string(this->color) + "m██\033[0m";
+        }        
     }
 
     void Colors::determineActivePiece(int colorMoving, int colorLocked){
